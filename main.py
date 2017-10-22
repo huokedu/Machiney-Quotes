61b9daab08144e7adba6ef60898aa943780768e1
Zach F <zach@zkf.me>
2017-10-22 17:40:36 -0400

    Latest page
    Added "latest" page; general cleanup; moved homepage to /

--- main.py
+++ main.py
@@ -1,98 +1,45 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask import render_template
-from random import randint
+from random import randint, shuffle
 from form import LoginForm
 
 app = Flask(__name__)
 app.config.from_object('config')
 Bootstrap(app)
 
-quotes = '''Yes is my fate in that mind is true.
-Try a little bit is enjoying any other than a flame
-The property, look like with stars shakes and actions
-You have he written to feel the spine men
-Be a phase is the straight of our human truth
-Why come into you get every money in dying
-Then what you will otherwise no make such mistakes
-Who you can't afford any day would write that'll each other upon my soul
-Ones needs a sings to be, taught, but as touched, for grace.
-Drink with their veil or a private refuge remains growth
-Achieve, and character's entire unity in his bicycle
-Who's it's a simple word for being
-Everything is always a good thing to the lyrics
-Safe was very much and stops a democratic world
-Why come that I find do really see the films
-Esteem in a single way we are a bad challenge
-Huge school, all ballroom writing and in the sea
-Achieve orders without being a precious of resentment or my league start
-Wow is pretty voyage and piano to terrorists our learning than brad meets
-Singing and the human part of, you know anything
-There remain ones to make people know anything in nor plays
-Never can't get any of those things now
-Repeat today or finish into a little old ocean'''.split('\n')
+with open("source/Quotes.txt") as f:
+    quotes = f.readlines()
 
-people = '''Marilyn Monroe
-Abraham Lincoln
-Mother Teresa
-John F. Kennedy
-Martin Luther King
-Nelson Mandela
-Winston Churchill
-Bill Gates
-Muhammad Ali
-Mahatma Gandhi
-Paul McCartney
-Plato
-Queen Elizabeth I
-Queen Victoria
-John M Keynes
-Mikhail Gorbachev
-Jawaharlal Nehru
-Leonardo da Vinci
-Louis Pasteur
-Leo Tolstoy
-Pablo Picasso
-Vincent Van Gogh
-Franklin D. Roosevelt
-Pope John Paul I
-Thomas Edison
-Rosa Parks
-Lyndon Johnson
-Oprah Winfrey
-Indira Gandhi
-Eva Peron
-Benazir Bhutto
-Desmond Tutu
-Dalai Lama
-Walt Disney
-Neil Armstrong
-Peter Sellers
-Barack Obama
-Malcolm X
-J.K.Rowling
-Richard Branson
-Angelina Jolie
-Jesse Owens
-Ernest Hemingway
-John Lennon
-Henry Ford
-Haile Selassie
-Ludwig Beethoven'''.split('\n')
+with open("source/People.txt") as f:
+    people = f.readlines()
 
-def gen_quote(_img):
+
+def gen_carousel_quote(_img = ''):
     return {'quote_text': quotes[randint(0, len(quotes) - 1)],
             'person': people[randint(0, len(people) - 1)],
-            'image': 'pic{}.jpg'.format(_img)}
+            'image': 'images/carousel/{}.jpg'.format(_img)}
 
-@app.route('/home')
+def gen_block_quote(_img = ''):
+    return {'quote_text': quotes[randint(0, len(quotes) - 1)],
+            'person': people[randint(0, len(people) - 1)],
+            'image': 'images/block/{}.jpg'.format(_img)}
+
+@app.route('/')
 def index():
-    quote_list = [gen_quote(i) for i in range(4,7)]
-    return render_template('home.html', quotes=quote_list)
+    quote_list = [gen_carousel_quote(i) for i in range(1,3)]
+    return render_template('index.html', quotes=quote_list)
+
+@app.route('/latest')
+def latest():
+    quote_images = range(1,9)
+    shuffle(quote_images)
+    quote_list = [gen_block_quote(quote_images[i]) for i in range(8)]
+    return render_template('list.html', quotes=quote_list)
 
 @app.route('/quote')
 def quote():
-    quote_list = [gen_quote(i) for i in range(4,7)]
+    quote_list = [gen_carousel_quote(i) for i in range(1,3)]
     return render_template('quote_page.html', quotes=quote_list)
 
 # Test for forms
